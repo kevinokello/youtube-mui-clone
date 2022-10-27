2391b03b5009a6b5b4f71222b8436f2fc3648753
kevinokello <okellokevin77@gmail.com>
2022-10-27 10:42:43 +0300

    youtube api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import { Button } from '@mui/material';
 import './App.css';
-
+import React from 'react';
 function App() {
+React.useEffect(() => {
+fetch(
+  "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAgQ_eJptXla5Qwha7V54tEmLi657mBvlI"
+).then((res) => res.json()).then((data) => console.log(data));
+}, []);
+
   return (
     <div className="App">
    <Button variant="contained">Test</Button>
